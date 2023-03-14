3315c23b9c395aad92649913e70aa7baa2c91799
dldlwotj <63402859+dldlwotj@users.noreply.github.com>
2023-03-14 16:39:30 +0900

    Create Volume_calc.py
    과제(1) - 부피 계산하는 프로그램

---
+++ Volume_calc.py
@@ -0,0 +1,7 @@
+width = float(input("width: "))
+length = float(input("length: "))
+height = float(input("height: "))
+
+volume = width * length * height
+
+print(f"The volume of the box is {volume}.")
